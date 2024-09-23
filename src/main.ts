62afe9c3556908a77507375ff5a39ed65e6f75c4
VicenteA18UCN <vicente.alarcon@alumnos.ucn.cl>
2024-09-23 20:48:38 -0300

    feat: add users module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Main APP | Taller 1 Arquitectura de Sistemas')
+    .setDescription('API para el taller 1 del ramo Arquitectura de Sistemas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
 }
 bootstrap();
