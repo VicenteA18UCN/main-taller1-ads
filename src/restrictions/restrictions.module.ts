fbe6cd03f723f7a0db7b287b1e1f9769e9b45d70
Vicente Alarcón <vicente.alarcon@alumnos.ucn.cl>
2024-09-24 00:32:44 -0300

    feat: add assign in restriction service

--- src/restrictions/restrictions.module.ts
+++ src/restrictions/restrictions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestrictionsService } from './restrictions.service';
 import { RestrictionsController } from './restrictions.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RestrictionsController],
   providers: [RestrictionsService],
 })
--- src/restrictions/restrictions.service.ts
+++ src/restrictions/restrictions.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 import { AssignRestrictionDto } from './dto/assign-restriction.dto';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class RestrictionsService {
-  assign(assignRestrictionDto: AssignRestrictionDto) {
-    console.log(assignRestrictionDto);
-    return 'This action adds a new restriction';
+  private readonly restrictionServiceUrl: string;
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.restrictionServiceUrl = this.configService.get<string>(
+      'URL_RESTRICTION_SERVICE',
+    );
+  }
+
+  async assign(assignRestrictionDto: AssignRestrictionDto) {
+    const { studentUuid, description } = assignRestrictionDto;
+
+    const url = `${this.restrictionServiceUrl}/students/${studentUuid}/restrictions`;
+
+    const response = await this.httpService
+      .post(url, { description })
+      .pipe(map((response) => response.data));
+
+    return response;
   }
 }
